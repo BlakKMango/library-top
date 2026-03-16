31611f98890a6bbba299829589656db03a981b12
Nike <198646766+BlakKMango@users.noreply.github.com>
2026-03-16 13:55:54 +0000

    Add function to prototype to dusplay the book info

--- js/script.js
+++ js/script.js
@@ -6,14 +6,27 @@ function Book(title, author, datePublished, genre) {
     this.author = author;
     this.datePublished = datePublished;
     this.genre = genre;
-    this.id = crypto.randomUUID;
+    this.id = crypto.randomUUID();
+}
+
+Book.prototype.info = function() {
+    return `${this.title} by ${this.author} (${this.published}) - ${this.genre}`;
 }
 
 function addBookToLibrary(title, author, datePublished, genre) {
     // take params, create a book then store it in the array
     myLibrary.push(new Book(title, author, datePublished, genre))
+    myLibrary.sort((a, b) => a.title.localeCompare(b.title));
+
+    displayLibrary()
 }
 
+function displayLibrary() {
+    myLibrary.forEach((book) => {
+        //create a new book element in the DOM
+        
+    })
+}
 
 
 addBookToLibrary
