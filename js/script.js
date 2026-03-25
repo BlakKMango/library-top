904261e2584f6101ba93f29d2124d220f928f91d
Nike <198646766+BlakKMango@users.noreply.github.com>
2026-03-25 10:47:53 +0000

    Refactored to use a class constructor

--- js/script.js
+++ js/script.js
@@ -4,34 +4,41 @@ const closeModalButton = document.querySelector("#close-modal");
 const openModalButton = document.querySelector("#open-modal");
 const newBookForm = document.querySelector("#new-book-form");
 
-
 const myLibrary = [];
 
-function Book(title, author, datePublished, genre, isbn) {
-    //create a constructor
-    this.title = title;
-    this.author = author;
-    this.datePublished = datePublished;
-    this.genre = genre;
-    this.isbn = isbn;
-    this.read = false;
-}
+class Book {
+    constructor(title, author, datePublished, genre, isbn) {
+        this.title = title;
+        this.author = author;
+        this.datePublished = datePublished;
+        this.genre = genre;
+        this.isbn = isbn;
+        this.read = false;
+    }
 
-Book.prototype.toggleRead = function() {
-    this.read = !this.read
-}
+    get bookInfo() {
+        const { title, author, datePublished, genre, isbn } = this;
+        return { title, author, datePublished, genre, isbn };
+    }
 
-Book.prototype.deleteBook = function() {
-    const index = myLibrary.indexOf(this);
-    myLibrary.splice(index, 1);
-}
+    //methods
+    toggleRead() {
+        this.read = !this.read
+    }
 
-function addBookToLibrary(title, author, datePublished, genre, isbn) {
-    // take params, create a book then store it in the array
-    myLibrary.push(new Book(title, author, datePublished, genre, isbn))
-    myLibrary.sort((a, b) => a.title.localeCompare(b.title));
+    deleteBook() {
+        const index = myLibrary.indexOf(this);
+        myLibrary.splice(index, 1);
+    }
 
-    displayBooks()
+    //Static - applied to class only
+    static addBookToLibrary(title, author, datePublished, genre, isbn) {
+        // take params, create a book then store it in the array
+        myLibrary.push(new Book(title, author, datePublished, genre, isbn))
+        myLibrary.sort((a, b) => a.title.localeCompare(b.title));
+
+        displayBooks()
+    }
 }
 
 function createBookCard(book) {
@@ -97,14 +104,6 @@ function displayBooks() {
     })
 }
 
-function deleteBookFromLibrary(isbn) {
-    //Find the book in the array
-    const index = myLibrary.findIndex(book => book.isbn === isbn);
-    myLibrary.splice(index, 1)
-    displayBooks();
-}
-
-
 //------------Modal controls -------------//
 function openModal() {
     addBookModal.showModal();
@@ -115,13 +114,12 @@ function closeModal() {
 }
 
 
-
 //--------------EVENT LISTENERS-----------//
 openModalButton.addEventListener("click", openModal)
 closeModalButton.addEventListener("click", closeModal)
 newBookForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    addBookToLibrary(
+    Book.addBookToLibrary(
         e.target.title.value,
         e.target.author.value,
         e.target.year.value,
@@ -133,19 +131,18 @@ newBookForm.addEventListener("submit", (e) => {
 })
 
 
-
-addBookToLibrary("All the Light We Cannot See", "Anthony Doerr", "2014", "historical-fiction", "9781476746586");
-addBookToLibrary("Anxious People", "Fredrik Backman", "2019", "literary-fiction", "9781405930253");
-addBookToLibrary("Beautiful World, Where Are You", "Sally Rooney", "2021", "literary-fiction", "9780571365425");
-addBookToLibrary("Conversations with Friends", "Sally Rooney", "2017", "literary-fiction", "9780571333134");
-addBookToLibrary("Crying in H Mart", "Michelle Zauner", "2021", "biography", "9780525657743");
-addBookToLibrary("Daisy Jones and The Six", "Taylor Jenkins Reid", "2019", "literary-fiction", "9781524798628");
-addBookToLibrary("Malibu Rising", "Taylor Jenkins Reid", "2021", "literary-fiction", "9781524798659");
-addBookToLibrary("Normal People", "Sally Rooney", "2018", "literary-fiction", "9780571334650");
-addBookToLibrary("Sunburn", "Chloe Michelle Hooper", "2024", "literary-fiction", "9780857308412");
-addBookToLibrary("The Handmaid's Tale", "Margaret Atwood", "1985", "dystopian", "9780385490818");
-addBookToLibrary("The Hunger Games", "Suzanne Collins", "2008", "dystopian", "9780439023481");
-addBookToLibrary("The Midnight Library", "Matt Haig", "2020", "literary-fiction", "9780525559474");
-addBookToLibrary("Where the Crawdads Sing", "Delia Owens", "2018", "literary-fiction", "9783453424012");
-addBookToLibrary("The Seven Husbands of Evelyn Hugo", "Taylor Jenkins Reid", "2017", "literary-fiction", "9783548066738");
-addBookToLibrary("Rubyfruit Jungle", "Rita Mae Brown", "1973", "literary-fiction", "9780553278866");
+Book.addBookToLibrary("All the Light We Cannot See", "Anthony Doerr", "2014", "historical-fiction", "9781476746586");
+Book.addBookToLibrary("Anxious People", "Fredrik Backman", "2019", "literary-fiction", "9781405930253");
+Book.addBookToLibrary("Beautiful World, Where Are You", "Sally Rooney", "2021", "literary-fiction", "9780571365425");
+Book.addBookToLibrary("Conversations with Friends", "Sally Rooney", "2017", "literary-fiction", "9780571333134");
+Book.addBookToLibrary("Crying in H Mart", "Michelle Zauner", "2021", "biography", "9780525657743");
+Book.addBookToLibrary("Daisy Jones and The Six", "Taylor Jenkins Reid", "2019", "literary-fiction", "9781524798628");
+Book.addBookToLibrary("Malibu Rising", "Taylor Jenkins Reid", "2021", "literary-fiction", "9781524798659");
+Book.addBookToLibrary("Normal People", "Sally Rooney", "2018", "literary-fiction", "9780571334650");
+Book.addBookToLibrary("Sunburn", "Chloe Michelle Hooper", "2024", "literary-fiction", "9780857308412");
+Book.addBookToLibrary("The Handmaid's Tale", "Margaret Atwood", "1985", "dystopian", "9780385490818");
+Book.addBookToLibrary("The Hunger Games", "Suzanne Collins", "2008", "dystopian", "9780439023481");
+Book.addBookToLibrary("The Midnight Library", "Matt Haig", "2020", "literary-fiction", "9780525559474");
+Book.addBookToLibrary("Where the Crawdads Sing", "Delia Owens", "2018", "literary-fiction", "9783453424012");
+Book.addBookToLibrary("The Seven Husbands of Evelyn Hugo", "Taylor Jenkins Reid", "2017", "literary-fiction", "9783548066738");
+Book.addBookToLibrary("Rubyfruit Jungle", "Rita Mae Brown", "1973", "literary-fiction", "9780553278866");
